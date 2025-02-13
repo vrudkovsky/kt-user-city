ab7c9d1867be04e42cf1ee842494f1f59f940601
Victor Rudkovsky <vpcomtek@gmail.com>
2025-02-13 02:05:55 +0200

    feat: User city prog

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "mate.academy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package mate.academy
+
+fun main() {
+    println("Enter your city ...")
+    val city = readLine()
+    println("User lives in $city")
+}
